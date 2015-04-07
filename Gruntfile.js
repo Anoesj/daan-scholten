f7db4dec5c8cfb538d7bd7ace8d2460e2ffe63bf
Anoesj Sadraee <anoesjsadraee@gmail.com>
2015-04-07 19:56:21 +0200

    bla

--- Gruntfile.js
+++ Gruntfile.js
@@ -253,6 +253,7 @@ module.exports = function (grunt) {
             // Usemin moves CSS and javascript inside of Usemin blocks.
             // Copy moves asset files and directories.
             'img/**/*',
+            'resources/img/**/*'
             'fonts/**/*',
             // Like Jekyll, exclude files & folders prefixed with an underscore.
             '!**/_*{,/**}',
