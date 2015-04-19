5caab8582a488affeafbba92f7fe138b5f4949a0
Anoesj Sadraee <anoesjsadraee@gmail.com>
2015-04-19 15:46:01 +0200

    poep

--- Gruntfile.js
+++ Gruntfile.js
@@ -407,8 +407,8 @@ module.exports = function (grunt) {
   ]);
 
   grunt.registerTask('deploy', [
-    'check',
-    'test',
+    // 'check',
+    // 'test',
     'build',
     'buildcontrol'
   ]);
